9b16f63b1db45ccc5f3306eaa7304a2781adf09a
rdpauw <depauwrobby@gmail.com>
2022-01-08 00:00:58 +0100

    add continuous functions and add readme

--- R/colors-and-helpers.R
+++ R/colors-and-helpers.R
@@ -1,9 +1,10 @@
-#' Sciensano Color Palettes
+#' Sciensano Discrete Color Palettes
 #'
 #' Color palettes inspired by housestyle of Sciensano \url{https://intranet.sciensano.be/nl/DienstenAlgemeenDirecteur/Communicatie/Pages/Huisstijl-en-templates.aspx}.
 #'
 #' @param palette Palette type.
-#' Currently there is one available option: \code{"default"}
+#' Currently there are two available option:
+#' \code{"default", "contrast"} containing all or a subset of the palette
 #' (14-color palette inspired by \emph{Sciensano}).
 #' @param alpha Transparency level, a real number in (0, 1].
 #' See \code{alpha} in \code{\link[grDevices]{rgb}} for details.
@@ -14,15 +15,16 @@
 #' @importFrom scales manual_pal
 #'
 #' @examples
-#' library("scales")
+#' \dontrun{library("scales")
 #' show_col(pal_sciensano("default")(10))
-#' show_col(pal_sciensano("default", alpha = 0.6)(10))
-pal_sciensano <- function(palette = c("default"), alpha = 1) {
+#' show_col(pal_sciensano("default", alpha = 0.6)(10))}
+pal_sciensano <- function(palette = c("default", "contrast"), alpha = 1) {
   palette <- match.arg(palette)
 
+  if (!(palette %in% c("default", "contrast"))) stop("palette should be one of the following: default, contrast")
   if (alpha > 1L | alpha <= 0L) stop("alpha must be in (0, 1]")
 
-  raw_cols <- sciensano_col_d
+  raw_cols <- sciensano_col_d[[palette]]
   raw_cols_rgb <- grDevices::col2rgb(raw_cols)
   alpha_cols <- grDevices::rgb(
     raw_cols_rgb[1L, ], raw_cols_rgb[2L, ], raw_cols_rgb[3L, ],
@@ -65,7 +67,7 @@ pal_sciensano <- function(palette = c("default"), alpha = 1) {
 #'   geom_histogram(colour = "black", binwidth = 1, position = "dodge") +
 #'   theme_bw() + scale_fill_sciensano()}
 
-scale_color_sciensano <- function(palette = c("default"), alpha = 1, ...) {
+scale_color_sciensano <- function(palette = c("default", "contrast"), alpha = 1, ...) {
   palette <- match.arg(palette)
   discrete_scale("colour", "sciensano", pal_sciensano(palette, alpha), ...)
 }
@@ -77,7 +79,131 @@ scale_colour_sciensano <- scale_color_sciensano
 #' @export scale_fill_sciensano
 #' @importFrom ggplot2 discrete_scale
 #' @rdname scale_sciensano
-scale_fill_sciensano <- function(palette = c("default"), alpha = 1, ...) {
+scale_fill_sciensano <- function(palette = c("default", "contrast"), alpha = 1, ...) {
   palette <- match.arg(palette)
   discrete_scale("fill", "sciensano", pal_sciensano(palette, alpha), ...)
+}
+
+#' Sciensano Continuous Color Palettes
+#'
+#' @param palette Palette type.
+#' There are 3 available options:
+#' \itemize{
+#' \item \code{"red"}
+#' \item \code{"green"}
+#' \item \code{"red-yellow-green"}}
+#' @param n Number of individual colors to be generated.
+#' @param alpha Transparency level, a real number in (0, 1].
+#' See \code{alpha} in \code{\link[grDevices]{rgb}} for details.
+#' @param reverse Logical. Should the order of the colors be reversed?
+#'
+#' @export rgb_sciensano_c
+#'
+#' @importFrom grDevices colorRamp rgb
+#' @importFrom scales manual_pal
+#' @importFrom grDevices colorRampPalette
+#'
+#' @examples
+#' \dontrun{library("scales")
+#' show_col(rgb_sciensano_c("red")(10))
+#' show_col(rgb_sciensano_c("red", n = 30, alpha = 0.6, reverse = TRUE)(30))}
+rgb_sciensano_c <- function(palette = c("red", "green", "red-yellow-green"),
+                            n = 10, alpha = 1, reverse = FALSE) {
+
+  palette <- match.arg(palette)
+
+  if (alpha > 1L | alpha <= 0L) stop("alpha must be in (0, 1]")
+
+  raw_cols <- sciensano_col_c[[palette]]
+  func_cols <- colorRamp(raw_cols, space = "Lab", interpolate = "spline")
+  mat_cols <- func_cols(seq(0L, 1L, length.out = n))
+  alpha_cols <- rgb(
+    mat_cols[, 1L], mat_cols[, 2L], mat_cols[, 3L],
+    alpha = alpha * 255L, maxColorValue = 255L
+  )
+
+  if (reverse) alpha_cols <- rev(alpha_cols)
+
+  alpha_cols
+}
+
+#' Sciensano Continuous Color Palettes
+#'
+#' @inheritParams rgb_sciensano_c
+#'
+#' @export pal_sciensano_c
+#'
+#' @importFrom scales manual_pal
+#'
+#' @examples
+#' \dontrun{library("scales")
+#' show_col(pal_sciensano("red-yellow-green")(10))
+#' show_col(pal_sciensano("red-yellow-green", n = 30, alpha = 0.6, reverse = TRUE)(30))}
+pal_sciensano_c <- function(
+  palette = c("red", "green", "red-yellow-green"),
+  n = 10, alpha = 1, reverse = FALSE) {
+  palette <- match.arg(palette)
+
+  alpha_cols <- rgb_sciensano_c(palette, n, alpha, reverse)
+  manual_pal(unname(alpha_cols))
+}
+
+#' Sciensano Continuous Color Palettes
+#'
+#' See \code{\link{pal_sciensano_c}} for details.
+#'
+#' @inheritParams pal_sciensano_c
+#' @param ... additional parameters for \code{\link[ggplot2]{discrete_scale}}
+#'
+#' @export scale_color_sciensano_c
+#'
+#' @importFrom ggplot2 scale_color_gradientn
+#'
+#' @rdname scale_sciensano_c
+#'
+#' @examples
+#' \dontrun{library("ggplot2")
+#' library("reshape2")
+#' data("mtcars")
+#'
+#' cor <- abs(cor(mtcars))
+#' cor_melt <- melt(cor)
+#'
+#' ggplot(
+#'   cor_melt,
+#'   aes(x = Var1, y = Var2, fill = value)
+#' ) +
+#'   geom_tile(colour = "black", size = 0.3) +
+#'   theme_bw() + scale_fill_sciensano_c("red-yellow-green")}
+scale_color_sciensano_c <- function(
+  palette = c("red", "green", "red-yellow-green"),
+  alpha = 1, reverse = FALSE, ...) {
+  palette <- match.arg(palette)
+  scale_color_gradientn(
+    colours = rgb_sciensano_c(
+      palette,
+      n = 512, alpha = alpha, reverse = reverse
+    ),
+    ...
+  )
+}
+
+#' @export scale_colour_sciensano_c
+#' @rdname scale_sciensano_c
+scale_colour_sciensano_c <- scale_color_sciensano_c
+
+#' @export scale_fill_sciensano_c
+#' @importFrom ggplot2 scale_fill_gradientn
+#' @rdname scale_sciensano_c
+scale_fill_sciensano_c <- function(
+  palette = c("red", "green", "red-yellow-green"),
+  alpha = 1, reverse = FALSE, ...) {
+  palette <- match.arg(palette)
+  scale_fill_gradientn(
+    colours = rgb_sciensano_c(
+      palette,
+      n = 512, alpha = alpha, reverse = reverse
+    ),
+    ...
+  )
 }
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -3,14 +3,14 @@
 ## create colour data
 
 ### discrete colours
-sciensano_col_d <-
+default <-
   c("midgreen" = rgb(red = 58, green = 170, blue = 53, maxColorValue = 255),
     "darkgreen" = rgb(red = 0, green = 102, blue = 51, maxColorValue = 255),
     "lightgreen" = rgb(red = 188, green = 207, blue = 0, maxColorValue = 255),
     "darkgrey" = rgb(red = 88, green = 89, blue = 91, maxColorValue = 255),
     "lightgrey" = rgb(red = 165, green = 165, blue = 165, maxColorValue = 255),
     "yellow" = rgb(red = 250, green = 213, blue = 0, maxColorValue = 255),
-    "orange" = rgb(red = 242, green = 213, blue = 0, maxColorValue = 255),
+    "orange" = rgb(red = 242, green = 157, blue = 0, maxColorValue = 255),
     "red" = rgb(red = 201, green = 81, blue = 23, maxColorValue = 255),
     "pink" = rgb(red = 228, green = 148, blue = 192, maxColorValue = 255),
     "violet" = rgb(red = 185, green = 50, blue = 135, maxColorValue = 255),
@@ -19,10 +19,36 @@ sciensano_col_d <-
     "greenblue" = rgb(red = 96, green = 188, blue = 184, maxColorValue = 255),
     "darkblue" = rgb(red = 34, green = 133, blue = 147, maxColorValue = 255))
 
-### continuos colours
+contrast <-
+  c("midgreen" = rgb(red = 58, green = 170, blue = 53, maxColorValue = 255),
+    "darkgreen" = rgb(red = 0, green = 102, blue = 51, maxColorValue = 255),
+    "lightgreen" = rgb(red = 188, green = 207, blue = 0, maxColorValue = 255),
+    "yellow" = rgb(red = 250, green = 213, blue = 0, maxColorValue = 255),
+    "orange" = rgb(red = 242, green = 157, blue = 0, maxColorValue = 255),
+    "red" = rgb(red = 201, green = 81, blue = 23, maxColorValue = 255),
+    "pink" = rgb(red = 228, green = 148, blue = 192, maxColorValue = 255),
+    "violet" = rgb(red = 185, green = 50, blue = 135, maxColorValue = 255),
+    "purple" = rgb(red = 86, green = 59, blue = 140, maxColorValue = 255),
+    "lighblue" = rgb(red = 132, green = 165, blue = 215, maxColorValue = 255),
+    "greenblue" = rgb(red = 96, green = 188, blue = 184, maxColorValue = 255),
+    "darkblue" = rgb(red = 34, green = 133, blue = 147, maxColorValue = 255))
+
+ sciensano_col_d <-
+   list("default" = default,
+        "contrast" = contrast)
+
+### continuous colours
+sciensano_col_c <-
+   list("green" = c("#00441B", "#F7FCF5"),
+        "red" = c("#CB181D", "#FFF5F0"),
+        "red-yellow-green" = c("#D73027", "#F46D43", "#FDAE61", "#FEE08B",
+                               "#FFFFBF", "#D9EF8B", "#A6D96A", "#66BD63",
+                               "#1A9850"))
+
 #### GREEN
 colfunc <- colorRampPalette(c("#00441B", "#F7FCF5"))
 scales::show_col(colfunc(100), labels = FALSE)
+
 
 #### RED
 colfunc <- colorRampPalette(c("#CB181D", "#FFF5F0"))
@@ -41,4 +67,4 @@ line_df <-
     lifeExp = rnorm(41, 75, 5))
 
 ## save data
-usethis::use_data(sciensano_col_d, line_df, internal = TRUE, overwrite = TRUE)
+usethis::use_data(sciensano_col_d, sciensano_col_c, line_df, internal = TRUE, overwrite = TRUE)
